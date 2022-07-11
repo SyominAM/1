1f36a79832c4bb7a5337d2540eb67b110d74dac6
SyominAM <seomin.aleksander2015@yandex.ru>
2022-07-11 09:47:16 +0300

    implementation method

---
+++ main.js
@@ -0,0 +1,78 @@
+'use strict'
+const eps = 0.01;
+let h = 1;
+let a = 3;
+let b = 3;
+let a_h = a;
+let b_h = b;
+let eps_out_page = (epssilon)=>{
+    let eps_show = document.getElementsByClassName('eps');
+    let eps_data = eps_show[0];
+    eps_data.innerHTML+=`Epsilon =${epssilon}`;
+}
+
+eps_out_page(eps);
+
+function calculate(x, y) {
+    return 6 * (x - y) - 3 * (Math.pow(x, 2)) - 3 * (Math.pow(y, 2));
+}
+
+
+function next_ai_calc(x, y, compare_result) {
+    if (compare_result < calculate(x + h, y)) {
+        a += h;
+        base_processing_result = calculate(a, b);
+    }
+    if (compare_result < calculate(x - h, y)) {
+        a -= h;
+        base_processing_result = calculate(a, b);
+    }
+
+}
+
+function next_bi_calc(x, y, compare_result) {
+    if (compare_result < calculate(x, y + h)) {
+        b += h;
+        base_processing_result = calculate(a, b);
+    }
+    if (compare_result < calculate(x, y - h)) {
+        b -= h;
+        base_processing_result = calculate(a, b);
+    }
+}
+
+function p_i_calc(x, x0) {
+    return x0 + 2 * (x - x0);
+}
+
+let base_processing_result;
+
+function jh_mehod_calc(){
+    let counter = 1;
+    base_processing_result = calculate(a, b);
+    do {
+        if (a == a_h && b == b_h) {
+            next_ai_calc(a, b, base_processing_result);
+            next_bi_calc(a, b, base_processing_result);
+            a = p_i_calc(a, a_h);
+            b = p_i_calc(b, b_h);
+        }
+    
+        if (calculate(a, b) < base_processing_result) {
+            base_processing_result = calculate(a, b);
+        } else if (calculate(a, b) > base_processing_result) {
+            
+            h=h/2;
+            a_h = a;
+            b_h = b;
+            
+        }
+ 
+        counter++;
+    } while (h > eps);
+    document.getElementById('res').innerHTML+=(`<hr><div class="function">f(x) = 6 * (x - y) - 3 * x<sup>2</sup> - 3 * y<sup>2</sup> = ${Math.round(base_processing_result)}</div>`);
+}
+
+
+
+
